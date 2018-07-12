4a34707083cf3253db9c21934f514c9d41419309
valeriacruise <valeriacruise22@gmail.com>
2018-07-12 13:44:12 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,9 +1,9 @@
 def usd_to_euro(money)
-  eur= (money*0.85605).round
+  eur= ("#{money}"*0.85605).round
 end
 
 def euro_to_usd(money)
-  usd= (money*1.168).round
+  usd= ("#{money}"*1.168).round
 end
 
 money(3)
